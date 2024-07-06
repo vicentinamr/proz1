0ab86cbd45ac3b85f323fee900df2bab95b926e4
vicentina martins dos reis <114178405+vicentinamr@users.noreply.github.com>
2024-07-05 22:18:36 -0300

    Add files via upload

---
+++ .py
@@ -0,0 +1,11 @@
+# Usando um laço for
+for andar in range(20, 0, -1):
+    if andar == 13:
+        continue
+    print(andar)
+# Usando um laço while
+andar = 20
+while andar > 0:
+    if andar != 13:
+        print(andar)
+    andar -= 1
